2ec3020174e0bac5bac1f546c6e2642c79bdc8a4
Anton Avhutskiy <41346887+muckduchok@users.noreply.github.com>
2025-04-16 19:33:51 +0300

    Create ln_drosera_config.sh

---
+++ ln_drosera_config.sh
@@ -0,0 +1,39 @@
+if ! command -v curl &> /dev/null; then
+    sudo apt update -y
+    sudo apt install curl -y
+fi
+
+PRIVATE=$(cat drosera_private.txt)
+
+curl -LO https://github.com/drosera-network/releases/releases/download/v1.16.2/drosera-operator-v1.16.2-x86_64-unknown-linux-gnu.tar.gz
+tar -xvf drosera-operator-v1.16.2-x86_64-unknown-linux-gnu.tar.gz
+sudo cp drosera-operator /usr/bin
+docker pull ghcr.io/drosera-network/drosera-operator:latest
+drosera-operator register --eth-rpc-url https://ethereum-holesky-rpc.publicnode.com --eth-private-key $PRIVATE
+
+sudo tee /etc/systemd/system/drosera.service > /dev/null <<EOF
+[Unit]
+Description=drosera node service
+After=network-online.target
+
+[Service]
+User=$USER
+Restart=always
+RestartSec=15
+LimitNOFILE=65535
+ExecStart=$(which drosera-operator) node --db-file-path $HOME/.drosera.db --network-p2p-port 31313 --server-port 31314 \
+    --eth-rpc-url https://ethereum-holesky-rpc.publicnode.com \
+    --eth-backup-rpc-url https://1rpc.io/holesky \
+    --drosera-address 0xea08f7d533C2b9A62F40D5326214f39a8E3A32F8 \
+    --eth-private-key $PRIVATE \
+    --listen-address 0.0.0.0 \
+    --network-external-p2p-address $(curl -4 -s ifconfig.me) \
+    --disable-dnr-confirmation true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable drosera
+sudo systemctl start drosera
