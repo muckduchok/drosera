aa89d09545a5beb466677a01c3f17dc587e8d6fe
Anton Avhutskiy <41346887+muckduchok@users.noreply.github.com>
2025-04-17 12:36:58 +0300

    Update ln_drosera_install.sh

--- ln_drosera_install.sh
+++ ln_drosera_install.sh
@@ -12,9 +12,9 @@ git config --global user.email "$EMAIL"
 git config --global user.name  "$USERNAME"
 
 $HOME/.foundry/bin/forge init -t drosera-network/trap-foundry-template
-HOME/.bun/bin/bun install
-HOME/.foundry/bin/forge build
+$HOME/.bun/bin/bun install
+$HOME/.foundry/bin/forge build
 
 ### ─────────────────────── 5. Apply trap ────────────────────────
 export DROSERA_PRIVATE_KEY=$PRIVATE
-echo ofc | HOME/.drosera/bin/drosera apply | tee drosera_ln.log | grep 'address:' > address_line.txt
+echo ofc | $HOME/.drosera/bin/drosera apply | tee drosera_ln.log | grep 'address:' > address_line.txt
