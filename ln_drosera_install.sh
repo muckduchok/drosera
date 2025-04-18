7e33d6c7ca717d1fe9af550a45e71d3ebca41631
Anton Avhutskiy <41346887+muckduchok@users.noreply.github.com>
2025-04-18 12:53:53 +0300

    Update ln_drosera_install.sh

--- ln_drosera_install.sh
+++ ln_drosera_install.sh
@@ -14,7 +14,6 @@ git config --global user.name  "$USERNAME"
 $HOME/.foundry/bin/forge init -t drosera-network/trap-foundry-template
 $HOME/.bun/bin/bun install
 $HOME/.foundry/bin/forge build
-sed -i 's|^ethereum_rpc = "https://ethereum-holesky-rpc\.publicnode\.com"|ethereum_rpc = "https://holesky.drpc.org/"|' drosera.toml
 ### ─────────────────────── 5. Apply trap ────────────────────────
 export DROSERA_PRIVATE_KEY=$PRIVATE
 echo ofc | $HOME/.drosera/bin/drosera apply | tee drosera_ln.log | grep 'address:' > address_line.txt
