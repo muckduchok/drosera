761d6c98100abff503c8df206456c156ffacce8e
Anton Avhutskiy <41346887+muckduchok@users.noreply.github.com>
2025-08-16 19:20:22 +0300

    Update update_hoodi.sh

--- update_hoodi.sh
+++ update_hoodi.sh
@@ -85,7 +85,7 @@ LimitNOFILE=65535
 ExecStart=$(which drosera-operator) node --db-file-path $HOME/.drosera.db --network-p2p-port 31313 --server-port 31314 \
     --eth-rpc-url https://ethereum-hoodi-rpc.publicnode.com \
     --eth-backup-rpc-url https://0xrpc.io/hoodi \
-    --drosera-address 0xea08f7d533C2b9A62F40D5326214f39a8E3A32F8 \
+    --drosera-address 0x91cB447BaFc6e0EA0F4Fe056F5a9b1F14bb06e5D \
     --eth-private-key $PRIVATE \
     --listen-address 0.0.0.0 \
     --network-external-p2p-address $(curl -4 -s ifconfig.me) \
